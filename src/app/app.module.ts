8aac3de5d345714deebcda615fcbd86d90e5d46f
Анастасія Кулинич <kulynychnastya@gmail.com>
2022-05-14 21:17:13 +0300

    homework 1 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user/user.component';
-import { UsersComponent } from './components/users/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
 import {HttpClientModule} from "@angular/common/http";
+import { PostComponent } from './components/post/post.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    PostComponent,
+    PostsComponent,
+    CommentsComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
