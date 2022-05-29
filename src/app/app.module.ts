c6f49df650e6c411cb4c8fa0ef6f837685eb024d
Анастасія Кулинич <kulynychnastya@gmail.com>
2022-05-29 23:09:56 +0300

    lesson 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainLayoutComponent,
+    CarsComponent,
+    CarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
