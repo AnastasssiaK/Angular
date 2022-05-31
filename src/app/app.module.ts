8e1d11d3e740686b31792122b06c2abbb5be84e1
Анастасія Кулинич <kulynychnastya@gmail.com>
2022-05-31 17:47:13 +0300

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { CustomPipe } from './services/pipes/custom.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CustomPipe
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
