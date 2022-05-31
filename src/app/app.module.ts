c1dc3e6640b779eed26d6076677a0da9ec465d87
Анастасія Кулинич <kulynychnastya@gmail.com>
2022-05-31 17:36:50 +0300

    emit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
+import {UsersComponent} from './components/users/users.component';
+import {UserComponent} from './components/user/user.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
